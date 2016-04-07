50bd0d0ba21dece6fc4d14961d97723ff3437720
tonyhu <tonyhu20116543@gmail.com>
2016-04-07 11:40:28 +0800

    tree depth on 7th April

---
+++ MaximumDepthOfBinaryTree.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == NULL)
+            return 0;
+        int n = maxDepth(root->left);
+        int m = maxDepth(root->right);
+        
+        return (n >= m ? n : m) + 1;
+    }
+};
