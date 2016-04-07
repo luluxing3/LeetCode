2090ebbeb5bde3e6711414a4056f472cc3acd8cb
tonyhu <tonyhu20116543@gmail.com>
2016-04-07 11:07:14 +0800

    * binarytree on 7th April

---
+++ ValidateBinarySearchTree.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    TreeNode pre = null;
+    
+    public boolean isValidBST(TreeNode root) {
+        if (root == null)
+            return true;
+        
+        if (!isValidBST(root.left))
+            return false;
+        if (pre != null && pre.val >= root.val)
+            return false;
+        pre = root;
+        if (!isValidBST(root.right))
+            return false;
+        
+        return true;
+    }
+}
