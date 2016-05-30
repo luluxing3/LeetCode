ff8d55fdce8d6ebf5a54ab0ac5f04973fbfa7e8c
tonyhu <tonyhu20116543@gmail.com>
2016-05-30 22:06:06 +0800

    array on May 30th

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0, j = 1;
+        while (j < nums.length) {
+            if (nums[i] == nums[j]) {
+                j++;
+            } else {
+                nums[++i] = nums[j++];
+            }
+        }
+        
+        return i + 1;
+    }
+}
