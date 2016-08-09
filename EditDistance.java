27d54446e77b35061585b3b1a4df913988324650
tonyhu <tonyhu20116543@gmail.com>
2016-08-09 11:11:48 +0800

    ** DP on August 9th

---
+++ EditDistance.java
@@ -0,0 +1,26 @@
+
+public class EditDistance {
+	
+	public int distance(String s1, String s2) {
+		int n = s1.length() + 1;
+		int m = s2.length() + 1;
+		int[][] d = new int[n][m];
+		for (int i = 0; i < n; i++)
+			d[i][0] = i;
+		for (int j = 0; j < m; j++)
+			d[0][j] = j;
+		
+		for (int i = 1; i <n; i++) {
+			for (int j = 1; j < m; j++) {
+				int tmp = Math.min(d[i-1][j] + 1, d[i][j-1] + 1);
+				if (s1.charAt(i - 1) != s2.charAt(j - 1)) {
+					d[i][j] = Math.min(tmp, d[i-1][j-1] + 1);
+				} else {
+					d[i][j] = Math.min(tmp, d[i-1][j-1]);
+				}
+			}
+		}
+		
+		return d[n - 1][m - 1];
+	}
+}
