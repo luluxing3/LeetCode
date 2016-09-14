54aedf5bf9903e5472b36fed2b5fa110f1a62305
tonyhu <tonyhu20116543@gmail.com>
2016-09-14 14:45:44 +0800

    ** DP on Sep 14th

--- WordBreak.java
+++ WordBreak.java
@@ -2,18 +2,15 @@ public class Solution {
     public boolean wordBreak(String s, Set<String> wordDict) {
         if (s == null || s.length() < 1 || wordDict == null || wordDict.size() < 1)
             return false;
-            
-        boolean[] mark = new boolean[s.length() + 1]; // if mark[i] = true, substring(0,i) can be broken into words
-        mark[0] = true;
-        for (int i = 1; i < s.length() + 1; i++) {
-            for (int j = 0; j < i; j++) {
-                String word = s.substring(j, i);
-                if (mark[j] && wordDict.contains(word)) {
-                    mark[i] = true;
-                    break;
-                }
-            }
+        
+        int[] d = new int [s.length() + 1]; // d[i] = 1表示字串[0,i)能划分成单词
+        d[0] = 1;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++)
+                if (d[j] == 1 && wordDict.contains(s.substring(j,i)))
+                    d[i] = 1;
         }
-        return mark[s.length()];
+        
+        return d[s.length()] == 1 ? true : false;
     }
 }
